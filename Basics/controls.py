613b787603d62de969f40819bb80d4d9331d72f5
matthcol <matthias@learning64.net>
2024-06-11 09:08:50 +0200

    end day 1

---
+++ Basics/controls.py
@@ -0,0 +1,16 @@
+population = 177000
+
+if population < 100000:
+    # block if condition is True
+    print("small city")
+else:
+    # block else: condition is False
+    print("big city")
+# end if
+
+for i in range(1000):
+    print("Spam")
+# end for
+
+print("The End")
+
