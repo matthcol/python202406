d480019584af101c7f9e465b392b5fdaa7ee138d
matthcol <matthias@learning64.net>
2024-06-11 15:16:54 +0200

    lists and tuples

---
+++ Basics/containers.py
@@ -0,0 +1,20 @@
+cities = [
+    "Toulouse",
+    "Montpellier",
+    "Marseille",
+    "Lyon",
+    "Valence",
+    "Saint Etienne",
+    "Paris",
+    "Pau",
+]
+
+city = "Toulouse", 477_000, "31000"
+
+liste_villes = [
+    ('Toulouse', 500_000, '31000'),
+    ('Agen', 200_000, '47000'),
+    ('Montauban', 100_000, '82000'),
+    ('Lyon', 700_000, '69000'),
+    ('Saint Etienne', 300_000, '42000'),
+]
