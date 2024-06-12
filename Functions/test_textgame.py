ced352f8173ebbac0160dc68aa81530b10defb6a
matthcol <matthias@learning64.net>
2024-06-12 15:08:11 +0200

    add 3 tests palindrome

--- Functions/test_textgame.py
+++ Functions/test_textgame.py
@@ -1,5 +1,17 @@
 from textgame import is_palindrome
 
 
-def test_is_palindrome():
-    assert is_palindrome("kayak")
+def test_is_palindrome_ci_ok():
+    assert is_palindrome("Kayak")
+
+
+def test_is_palindrome_ci_ko():
+    assert not is_palindrome("python")
+
+
+def test_is_palindrome_cs_ok():
+    assert is_palindrome("kayak", case=True)
+
+
+def test_is_palindrome_cs_ko():
+    assert not is_palindrome("Kayak", case=True)
