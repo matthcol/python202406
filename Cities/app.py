b155ea6ac647cecdfa44192a4f73316d4897f396
matthcol <matthias@learning64.net>
2024-06-13 13:38:03 +0200

    cities

--- Cities/app.py
+++ Cities/app.py
@@ -12,6 +12,21 @@ city5 = city1.copy()
 city1.population += 1_000
 
 for city in city1, city2:
-    print(city)
+    print(city, str(city))
+    print(repr(city))
+    print(f"a city {city} with its str conversion.")
+    print(f"a city {city!r} with its representation.")
     print(city.name, city.population, city.code_postal)
     print()
+
+print("city1 == city2:", city1 == city2)
+print("city1 != city2:", city1 == city2)
+print("city1 == city3 (same ref):", city1 == city3)
+print("city4 == city5 (clones):", city4 == city5)
+
+# with @dataclass:
+# TypeError: '<' not supported between instances of 'City' and 'City'
+# with @dataclass(order=True)
+print("city1 < city2:", city1 < city2) # False 'Toulouse' > 'Lyon'
+print("city3 < city1:", city3 < city1) # False: same
+print("city4 < city1:", city4 < city1) # True: name ==, pop4 < pop1
--- Cities/city.py
+++ Cities/city.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
 
-
-@dataclass
+# decorator @dataclass generates:
+#   __init__ (constructor): City(...)
+#   __repr__ (and __str__ by default): builtins str(), repr(), print, f-string
+#   __eq__ (equals)  __ne__ (not equals): operators == and !=
+# (opt)  __lt__ (less than) + __le__ + __gt__ + __ge__: operators < <= > >=
+@dataclass(order=True)
 class City:
     # attributes
     name: str
@@ -9,5 +13,10 @@ class City:
     code_postal: str
 
     # methods
+
+    def __str__(self):
+        return f"{self.name} ({self.code_postal}, {self.population} hab)"
+
+
     def copy(self):
         return City(name=self.name, population=self.population, code_postal=self.code_postal)
